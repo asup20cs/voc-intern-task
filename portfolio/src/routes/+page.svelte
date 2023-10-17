500aa73936f458454ab003e3997f2bda0c011241
Ashutosh Khanal Upadhyay <89827229+asup20cs@users.noreply.github.com>
2023-10-17 08:33:40 +0530

    configured week 2 task

---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Modal from "../components/modal-close.svelte";
+    import Menu from "../components/menu.svelte";
+    import Card from "../components/card-badge.svelte";
+</script>
+<div class="container">
+    <Card />
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+</style>
